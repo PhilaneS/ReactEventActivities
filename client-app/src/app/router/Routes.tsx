24dca432b4d591d42394195b2b7097184365f1bf
Philane Sigwebela <kaizer.pk@gmail.com>
2024-05-02 23:30:20 +0200

    Added Error Handling to API and Client Application

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,8 +1,11 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import App from "../layout/App";
+import TestErrors from "../../features/activities/errors/TestErrors";
+import NotFound from "../../features/activities/errors/NotFound";
+import ServerError from "../../features/activities/errors/ServerError";
 
 export const routes: RouteObject[] = [
     {
@@ -11,8 +14,11 @@ export const routes: RouteObject[] = [
         children: [
             {path: 'activities', element: <ActivityDashboard />},
             {path: 'activities/:id', element: <ActivityDetails />},
-            {path: 'createActivity', element: <ActivityForm key='create' />},
-            {path: 'manage/:id', element: <ActivityForm key='manage' />},
+            {path: 'createActivity', element: <ActivityForm key='create' />},            
+            {path: 'errors', element: <TestErrors key='errors' />},
+            {path: 'not-found', element: <NotFound />},
+            {path: 'server-error', element: <ServerError />},
+            {path: '*', element: <Navigate replace to='/not-found' />},
         ]
     }
 ]
