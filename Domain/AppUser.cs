da856bf81b12697e853064d7f731a3b67b355a6f
Philane Sigwebela <kaizer.pk@gmail.com>
2024-10-11 23:39:22 +0200

    Added Following entity and migration

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -11,8 +11,8 @@ namespace Domain
 
         // public ICollection<UserActivity> UserActivities { get; set; }
         // public ICollection<Photo> Photos { get; set; }
-        // public ICollection<UserFollowing> Followings { get; set; }
-        // public ICollection<UserFollowing> Followers { get; set; }
+        public ICollection<UserFollowing> Followings { get; set; }
+        public ICollection<UserFollowing> Followers { get; set; }
         
     }
 }
